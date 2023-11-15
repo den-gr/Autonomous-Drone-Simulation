a806c852ce1ce9563703bac59788c0ce081cf45f
Denys <denys.grushchak@studio.unibo.it>
2023-11-15 16:46:23 +0100

    squah gitignore commits

--- app/src/main/resources/bounding_boxes.py
+++ app/src/main/resources/bounding_boxes.py
@@ -1,59 +1,52 @@
 # %%
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import numpy as np
 import matplotlib.animation as animation
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import pandas as pd
-import random
 
 file_path = 'data/12_01_23-DJI_0994.csv'
 
 df = pd.read_csv(file_path)
 
-def remove_noise(df, filter_window=3):
+def remove_noise(df, filter_window=3, print=False):
+    LONGITUDE_CENTER = 37
     cond1 = abs(df['latitude']) < filter_window
-    cond2 = abs(df["longitude"] - 37) < filter_window
+    cond2 = abs(df["longitude"] - LONGITUDE_CENTER) < filter_window
     condition = cond1 & cond2
-    print("Noise coordinates:")
-    print((df[~condition])[["latitude", "longitude"]])
+    if(print):
+        print("Noise coordinates:")
+        print((df[~condition])[["latitude", "longitude"]])
     return df[condition]
 
 df = remove_noise(df)
 
+# Get Zebras data and drone postion grouped by frames
 def get_drone_and_zebras_coords(df):
-    dff = df[['frame', 'latitude', 'longitude', "altitude", "id", "xtl", "ytl", "xbr", "ybr", "behaviour"]]
+    dff = df.loc[:, ['frame', 'latitude', 'longitude', "altitude", "id", "xtl", "ytl", "xbr", "ybr", "behaviour"]]
     dff["box"] = dff.apply(lambda row: (row["xtl"], row["ytl"], row["xbr"], row["ybr"], row["id"], row["behaviour"]), axis=1)
-    # print(dff["box"])
     dff = dff.groupby(["frame", "latitude", "longitude", "altitude"])["box"].apply(list).reset_index(name='boxes')
-    # dff["id_arrays"] = dff["id_arrays"].apply(sorted)
     dff = dff[['latitude', 'longitude', 'altitude', "boxes"]]
 
     return dff.values.tolist()
 
-
 boxes = get_drone_and_zebras_coords(df)
 
+# %% Animation creation
 
-
-# %% craetion Animation
-
-width, height = 3840, 2160
 plt.rcParams['animation.ffmpeg_path'] = 'ffmpeg'
+width, height = 3840, 2160
 fig, ax = plt.subplots(figsize=(width / 100, height / 100), dpi=100)
-
 ax.set_xlim(0, width)
 ax.set_ylim(0, height)
 ax.set_facecolor('white')
-# ax.get_legendremove()
 ax.axis("off")
 
-def plot_bounding_box2(xtl, ytl, xbr, ybr): 
+def create_rectangle(xtl, ytl, xbr, ybr): 
     return patches.Rectangle((xtl, ybr), xbr - xtl, ytl - ybr, linewidth=4, edgecolor='g', facecolor='none')
 
 box = boxes[0][3][0]
-ax.add_patch(plot_bounding_box2(box[0], box[1], box[2], box[3]))
+ax.add_patch(create_rectangle(box[0], box[1], box[2], box[3]))
 
 def animate(f_id):
     for patch in ax.patches:
@@ -62,18 +55,20 @@ def animate(f_id):
         text.remove()
     
     for box in boxes[f_id][3]:
+        # Original coordinates has (0; 0) in top left corner
+        # Mathplot (0; 0) is in bottom left corner
         xtl, ytl, xbr, ybr, id, behaviour = box
-        ytl = height - ytl
+        # invert top and bottom
+        ytl = height - ytl 
         ybr = height - ybr
-        rect = plot_bounding_box2(xtl, ytl, xbr, ybr)
-        ax.add_patch(rect)
+        ax.add_patch(create_rectangle(xtl, ytl, xbr, ybr))
         text_x = (xtl + xbr) / 2
         text_y = (ytl + ybr) / 2
-        ax.text(text_x, text_y, str(int(id)), color='black', ha='center', va='center', fontsize=17, bbox=dict(facecolor='white', edgecolor='white', boxstyle='round,pad=0.5'))
-        ax.text(text_x, ytl + 40, behaviour, color='black', ha='center', va='center', fontsize=17, bbox=dict(facecolor='white', edgecolor='white', boxstyle='round,pad=0.5'))
+        ax.text(text_x, text_y, str(int(id)), color='black', ha='center', va='center', fontsize=17)
+        ax.text(text_x, ytl + 40, behaviour, color='black', ha='center', va='center', fontsize=17)
     ax.set_title("(" + str(boxes[f_id][0]) + "; " + str(boxes[f_id][1]) + ") | Altitude: " + str(boxes[f_id][2]), fontsize=30)
 
 
-ani = animation.FuncAnimation(fig, animate, frames=len(boxes))
-FFwriter = animation.FFMpegWriter(fps=60)
-ani.save('drone_bonded_boxes_60pfs.mp4', writer=FFwriter)
+ani = animation.FuncAnimation(fig, animate, frames=90)
+FFwriter = animation.FFMpegWriter(fps=30)
+ani.save('data/drone_bonded_boxes_60pfs.mp4', writer=FFwriter)
